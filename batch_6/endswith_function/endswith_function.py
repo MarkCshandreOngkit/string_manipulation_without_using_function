895a1171b392d3a3ddc750f8269d519776c923cb
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:03:46 +0800

    check conditions and print

--- batch_6/endswith_function/endswith_function.py
+++ batch_6/endswith_function/endswith_function.py
@@ -2,12 +2,20 @@
 string = input("Input: ")
 #input end_string
 end_string = input("Check if ending in: ")
+
 #find end_string in string
 position = string.rfind(end_string)
-print(position)
-#if endstring not in string
-    #print False
-#if endstring in string but not in end
-    #print False
-#if endstring in string in end position
+
+#check length of string and end_string(for checking if end string is in the end)
+string_length = len(string)
+end_string_length = len(end_string)
+
+
+#check if end_string is in the end of string
+if position + end_string_length == string_length:
     #print True
+    print("Output: True")
+#if not
+else:
+    #print False
+    print("Output: False")
