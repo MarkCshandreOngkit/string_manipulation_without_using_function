d430ad65e3399893a7a5431699304b84e809aec1
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:52:34 +0800

    pseudocode

---
+++ batch_6/endswith_function/endswith_function.py
@@ -0,0 +1,9 @@
+#input string
+#input end_string
+#find end_string in string
+#if endstring not in string
+    #print False
+#if endstring in string but not in end
+    #print False
+#if endstring in string in end position
+    #print True
