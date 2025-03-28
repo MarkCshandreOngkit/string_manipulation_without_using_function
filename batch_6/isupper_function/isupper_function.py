f0954f605266446a4f4c28f71fe46663899d301d
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:41:37 +0800

    check if uppercase and print

--- batch_6/isupper_function/isupper_function.py
+++ batch_6/isupper_function/isupper_function.py
@@ -1,7 +1,12 @@
 #input string
 string = input("Input: ")
 #create upper variable with string converted to upper
+upper = string.upper()
 #check if string is the same as upper
+if string == upper:
     #print True
+    print("Output: True")
 #if not
+else:
     #print False
+    print("Output: False")
