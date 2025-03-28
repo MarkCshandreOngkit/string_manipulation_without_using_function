6f1beba154dee39232a4f04002e41f68a6e63ca7
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:52:54 +0800

    fill spaces and print

--- batch_6/center_function/center_function.py
+++ batch_6/center_function/center_function.py
@@ -3,7 +3,7 @@ import math
 #input string
 string = input("Input: ")
 #input length of string to be returned(max_length)
-max_length = input("Max Length of String: ")
+max_length = int(input("Max Length of String: "))
 
 #get length of string
 string_length = len(string)
@@ -12,6 +12,13 @@ filler = max_length - string_length
 #get half of filler(ceiling)
 half_filler = math.ceil(filler / 2)
 
-#fill beginning of string with zero
-#fill end of string with zero
+#fill beginning of string with " "
+beginning_filler = " " * half_filler
+string = beginning_filler + string
+
+#fill end of string with " "
+end_filler = " " * (filler - half_filler)
+string = string + end_filler
+
 #print string
+print(string)
