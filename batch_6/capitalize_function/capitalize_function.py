cdc002bec80b56463ddcb95f050c2ebbb8898feb
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:07:17 +0800

    input and for loop

--- batch_6/capitalize_function/capitalize_function.py
+++ batch_6/capitalize_function/capitalize_function.py
@@ -1,5 +1,8 @@
 #input string
+string = input("Input: ")
 #iterate through each character
+for character in string:
+    print(character)
     #check if character is the first
         #convert to upper
     #else
