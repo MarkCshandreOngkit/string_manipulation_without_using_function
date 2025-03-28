d5772acc02bb375d9164997d7157ba325d485dd5
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:33:06 +0800

    import and inputs

--- batch_6/center_function/center_function.py
+++ batch_6/center_function/center_function.py
@@ -1,6 +1,10 @@
 #import math
+import math
 #input string
+string = input("Input: ")
 #input length of string to be returned(max_length)
+max_length = input("Max Length of String: ")
+
 #get length of string
 #get space filler(max_length - string_length)
 #get half of filler(ceiling)
