5caa32decbae1a06de88bbf3f1d198af6d700a12
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:09:43 +0800

    check space, remove space on the left only, and print output

--- batch_6/lstrip_function/lstrip_function.py
+++ batch_6/lstrip_function/lstrip_function.py
@@ -1,10 +1,16 @@
 #input a string(words, statements, etc.)
 string = input("Input: ")
+
 #iterate through each character in the string
 for character in string:
-    print(character)
     #check if the character is a space character
+    if character == " ":
         #replace space with "" and save if true
+        string = string.removeprefix(" ")
     #check if the character is false
+    else:
         #stop looping
+        break
 #print output
+print(string)
+        
