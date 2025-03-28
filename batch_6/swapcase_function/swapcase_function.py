6beda7c07e9a8da616f2572769ea6ef552dc22ba
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:58:15 +0800

    pseudocode

---
+++ batch_6/swapcase_function/swapcase_function.py
@@ -0,0 +1,11 @@
+#input string
+#iterate through each character
+#check if lower
+    #convert to upper if true
+    #add to new string
+#if upper
+    #convert to lower if true
+    #add to new string
+#else
+    #add to new string
+#print new string
