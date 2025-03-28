06563977dc9272b3c4e484c496efb1256f22f7e5
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:20:50 +0800

    inputs

--- batch_6/removeprefix_function/removeprefix_function.py
+++ batch_6/removeprefix_function/removeprefix_function.py
@@ -1,5 +1,7 @@
 #input string
+string = input("Input: ")
 #input prefix to be remove if in the beginning of string
+prefix = input("Prefix to be remove: ")
 #check if string starts with prefix
     #replace prefix with "" 1 time
 #print string
