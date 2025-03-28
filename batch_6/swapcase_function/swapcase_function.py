23fbcf32387e30a88c95e8110159a6449436c6fe
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:03:14 +0800

    check conditions and print

--- batch_6/swapcase_function/swapcase_function.py
+++ batch_6/swapcase_function/swapcase_function.py
@@ -1,14 +1,24 @@
 #input string
 string = input("Input: ")
+#initialize
+new_string = ""
 #iterate through each character
 for character in string:
-    print(character)
-#check if lower
-    #convert to upper if true
-    #add to new string
-#if upper
-    #convert to lower if true
-    #add to new string
-#else
-    #add to new string
+    #check if lower
+    if character.islower():
+        #convert to upper if true
+        character = character.upper()
+        #add to new string
+        new_string += character
+    #if upper
+    elif character.isupper():
+        #convert to lower if true
+        character = character.lower()
+        #add to new string
+        new_string += character
+    #else
+    else:
+        #add to new string
+        new_string += character
 #print new string
+print(f"Output: {new_string}")
