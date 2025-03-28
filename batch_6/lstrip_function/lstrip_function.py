3405920bd2231324258af02a7612be31d1644496
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 16:59:59 +0800

    input string and iterate through each character

--- batch_6/lstrip_function/lstrip_function.py
+++ batch_6/lstrip_function/lstrip_function.py
@@ -1,5 +1,8 @@
 #input a string(words, statements, etc.)
+string = input("Input: ")
 #iterate through each character in the string
+for character in string:
+    print(character)
     #check if the character is a space character
         #replace space with "" and save if true
     #check if the character is false
