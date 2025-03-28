f35eb640b410eb70b3dbf55aef216b3cc4e9aa9f
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:28:13 +0800

    check conditions and print output

--- batch_6/title_function/title_function.py
+++ batch_6/title_function/title_function.py
@@ -2,17 +2,32 @@
 string = input("Input: ")
 #split string into words
 words = string.split()
-print(words)
+
+#initialize
+first = True
+new_string = ""
+
 #iterate through each words
 for word in words:
     #iterate through each characters
     for character in word:
-        print(character)
         #check if first letter
+        if character.isalpha() and first:
             #convert to upper if true
+            character = character.upper()
             #add to new string
+            new_string += character
+            first = False
+
         #else
+        else:
             #convert to lower 
+            character = character.lower()
             #add to new string
+            new_string += character
     #add space to new string
+    new_string += " "
+    first = True
+
 #print new string
+print(f"Output: {new_string}")
