973da7292315f59272f8b2076323bfced4f5662c
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:35:16 +0800

    math processes for getting center

--- batch_6/center_function/center_function.py
+++ batch_6/center_function/center_function.py
@@ -6,7 +6,12 @@ string = input("Input: ")
 max_length = input("Max Length of String: ")
 
 #get length of string
+string_length = len(string)
 #get space filler(max_length - string_length)
+filler = max_length - string_length
 #get half of filler(ceiling)
+half_filler = math.ceil(filler / 2)
+
 #fill beginning of string with zero
 #fill end of string with zero
+#print string
