6b4113a74a23714b825643ec0007ba313a86bd33
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:14:28 +0800

    inputs

--- batch_6/ljust_function/ljust_function.py
+++ batch_6/ljust_function/ljust_function.py
@@ -1,5 +1,7 @@
 #input string
+string = input("Input: ")
 #input length of string to be returned(length_returned)
+length_returned = input("Max Length of String: ")
 #fill beginning of string with "0"
 #replace "0" with " "
 #print string
