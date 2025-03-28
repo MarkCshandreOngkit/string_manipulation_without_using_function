fcde10ba09b3005b323c1c90c716b639ad10666b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:11:08 +0800

    process string and print

--- batch_6/capitalize_function/capitalize_function.py
+++ batch_6/capitalize_function/capitalize_function.py
@@ -1,10 +1,20 @@
 #input string
 string = input("Input: ")
+#initialize
+new_string = ""
 #iterate through each character
 for character in string:
-    print(character)
     #check if character is the first
+    if character == string[0]:
         #convert to upper
+        character = character.upper()
+        #add to new string
+        new_string += character
     #else
+    else:
         #convert to lower
+        character = character.lower()
+        #add to new string
+        new_string += character
 #print output
+print(f"Output: {new_string}")
