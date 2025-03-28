d7a24623abcb95a87d3ca455c5699b7593becad4
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:24:17 +0800

    check if prefix match, remove if true, and print

--- batch_6/removeprefix_function/removeprefix_function.py
+++ batch_6/removeprefix_function/removeprefix_function.py
@@ -3,5 +3,8 @@ string = input("Input: ")
 #input prefix to be remove if in the beginning of string
 prefix = input("Prefix to be remove: ")
 #check if string starts with prefix
+if string.startswith(prefix):
     #replace prefix with "" 1 time
+    string = string.replace(prefix, "", 1)
 #print string
+print(string)
