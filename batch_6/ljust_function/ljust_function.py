d5c6c7c39a2f3885b515a056f566a68e73151aeb
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:22:11 +0800

    fill zero, replace to space, and print

--- batch_6/ljust_function/ljust_function.py
+++ batch_6/ljust_function/ljust_function.py
@@ -1,7 +1,15 @@
 #input string
 string = input("Input: ")
 #input length of string to be returned(length_returned)
-length_returned = input("Max Length of String: ")
+length_returned = int(input("Max Length of String: "))
+
+#get length of string
+string_length = len(string)
+
 #fill beginning of string with "0"
+string = string.zfill(length_returned)
 #replace "0" with " "
+string = string.replace("0", " ", length_returned - string_length)
+
 #print string
+print(f"Output:\n>{string}")
