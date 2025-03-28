b5f31a2c9981cadbfa20e4e94207fe1b9c64b80e
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:19:29 +0800

    pseudocode

---
+++ batch_6/title_function/title_function.py
@@ -0,0 +1,12 @@
+#input string
+#split string into words
+#iterate through each words
+    #iterate through each characters
+        #check if first letter
+            #convert to upper if true
+            #add to new string
+        #else
+            #convert to lower 
+            #add to new string
+    #add space to new string
+#print new string
