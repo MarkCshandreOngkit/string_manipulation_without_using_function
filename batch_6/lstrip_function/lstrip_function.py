19191cb5a13695c214e8ef4d47ae14fdeed0043e
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 16:54:03 +0800

    pseudocode

---
+++ batch_6/lstrip_function/lstrip_function.py
@@ -0,0 +1,6 @@
+#iterate through each character in the string
+    #check if the character is a space character
+        #replace space with "" and save if true
+    #check if the character is false
+        #stop looping
+#print output
