90bff15b7fd9daddcd491086509c4a022ffaadf5
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 18:31:49 +0800

    pseudocode

---
+++ batch_6/center_function/center_function.py
@@ -0,0 +1,8 @@
+#import math
+#input string
+#input length of string to be returned(max_length)
+#get length of string
+#get space filler(max_length - string_length)
+#get half of filler(ceiling)
+#fill beginning of string with zero
+#fill end of string with zero
