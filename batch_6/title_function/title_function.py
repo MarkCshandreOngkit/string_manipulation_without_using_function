351f26d33c0917b32c057cd00644489894835ec6
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 19:21:17 +0800

    input and split string

--- batch_6/title_function/title_function.py
+++ batch_6/title_function/title_function.py
@@ -1,5 +1,8 @@
 #input string
+string = input("Input: ")
 #split string into words
+words = string.split()
+print(words)
 #iterate through each words
     #iterate through each characters
         #check if first letter
