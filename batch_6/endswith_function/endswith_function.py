b01eb548a67a863654d7b35f927d58d072c1614e
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-28 17:55:42 +0800

    find position if in string

--- batch_6/endswith_function/endswith_function.py
+++ batch_6/endswith_function/endswith_function.py
@@ -3,6 +3,8 @@ string = input("Input: ")
 #input end_string
 end_string = input("Check if ending in: ")
 #find end_string in string
+position = string.rfind(end_string)
+print(position)
 #if endstring not in string
     #print False
 #if endstring in string but not in end
